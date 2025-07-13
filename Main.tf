454c8e4d1923cd204e02beaa97efc654ede25a83
nitin08103 <nitin08103@gmail.com>
2025-07-13 12:28:25 +0530

    Create Main.tf
    Create code for one RG by main branch

---
+++ Main.tf
@@ -0,0 +1,26 @@
+# -----------------------------
+# Required Providers Block
+# -----------------------------
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# -----------------------------
+# Provider Configuration
+# -----------------------------
+provider "azurerm" {
+  features {}
+}
+
+# -----------------------------
+# Resource Group Definition
+# -----------------------------
+resource "azurerm_resource_group" "example" {
+  name     = "example-resource-group"
+  location = "East US"
+}
